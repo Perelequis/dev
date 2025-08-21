e6b609ed404e4a1fa2bde9d53c9099aa79d5768a
perelequis <168141570+perelequis@users.noreply.github.com>
2025-08-21 16:33:03 -0300

    Update eventos2024.js

--- calendario/eventos2024.js
+++ calendario/eventos2024.js
@@ -1,5 +1,26 @@
-'2024-0-15': [
-  { title: 'Reunião 9h', link: 'https://meet.google.com' },
-  { title: 'Almoço 12h', link: 'https://restaurante.com' },
-  { title: 'Apresentação 15h', link: 'https://slides.com' }
-]
+{
+    // ===== JANEIRO 2024 =====
+    '2024-0-1': { title: 'Ano Novo', link: 'https://www.google.com' },
+    '2024-0-6': { title: 'Dia de Reis', link: '' },
+    '2024-0-15': [
+        { title: 'Reunião 9h', link: 'https://meet.google.com' },
+        { title: 'Almoço 12h', link: '' },
+        { title: 'Cinema 20h', link: 'https://ingresso.com' }
+    ],
+    '2024-0-25': { title: 'Aniversário João', link: 'https://instagram.com/joao' },
+
+    // ===== FEVEREIRO 2024 =====
+    '2024-1-14': { title: 'Dia dos Namorados', link: 'https://flores.com' },
+    '2024-1-20': [
+        { title: 'Dentista 14h', link: '' },
+        { title: 'Academia 18h', link: 'https://smartfit.com.br' }
+    ],
+
+    // ===== MARÇO 2024 =====
+    '2024-2-8': { title: 'Dia da Mulher', link: 'https://diadamulher.com' },
+    '2024-2-15': { title: 'Reunião trabalho', link: '' },
+
+    // ===== DEZEMBRO 2024 =====
+    '2024-11-25': { title: 'Natal', link: 'https://papainoel.com' },
+    '2024-11-31': { title: 'Réveillon', link: 'https://festa.com' }
+}
