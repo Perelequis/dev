5661890e93af1e5d9833a18f085be337bbf1c57d
perelequis <168141570+perelequis@users.noreply.github.com>
2025-08-21 13:07:28 -0300

    Create eventos2024.js

---
+++ calendario/eventos2024.js
@@ -0,0 +1,5 @@
+'2024-0-15': [
+  { title: 'Reunião 9h', link: 'https://meet.google.com' },
+  { title: 'Almoço 12h', link: 'https://restaurante.com' },
+  { title: 'Apresentação 15h', link: 'https://slides.com' }
+]
